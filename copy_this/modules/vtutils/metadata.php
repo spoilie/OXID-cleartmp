98354a2218a6e112ea228b0cd56ac9e44a9a4e51
Joscha Krug <krug@marmalade.de>
2012-07-17 12:40:36 +0200

    Added metadata.php for compatibility and easy installation in OXID v4.6+

---
+++ copy_this/modules/vtutils/metadata.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * VTUtils - Clear TMP
+ *
+ * PHP version 5
+ *
+ * @category vtutils
+ * @package  vtutils
+ * @author   Marat Bedoev | HEINER DIRECT GmbH & Co KG
+ * @license  GPL License V3.0 http://www.opensource.org/licenses/gpl-license.html
+ * @version  GIT: <$Id$>
+ */
+
+$aModule = array(
+    'id'          => 'vtutils',
+    'title'       => 'VTUtils Clear TMP',
+	'description' => 'Clear TMP directory from the backend',
+	'version'     => '1.0',
+	'author'      => 'Marat Bedoev | HEINER DIRECT GmbH & Co KG',
+	'extend'      => array(
+	    'navigation' => 'vtutils/navigation_ext',
+	)
+);
